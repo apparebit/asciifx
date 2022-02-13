87de29112a9957411a67bc590204c13b323366c8
Robert Grimm <apparebit@gmail.com>
2022-02-13 04:28:58 -0500

    Move examples into nested directory

--- example/input.py
+++ example/input.py
@@ -1,10 +1,10 @@
+#[keypress-speed=0.5]
 #[off]
 import konsole
 konsole.config(use_color=True)
 #[on]
 konsole.info("fyi", detail=["for", "your", "information"])
 konsole.warning("beware")
-#[think=0.3]
 konsole.error("bad")
 konsole.critical("enough already!!!")
 quit()
