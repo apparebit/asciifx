2b76281d378efec9e89402fe5fed04b40b5c63d0
Robert Grimm <apparebit@gmail.com>
2022-02-15 10:47:32 -0500

    Make classes configurable, return minimum screen size, automate setting of screen size in header

--- asciifx/perform.py
+++ asciifx/perform.py
@@ -1,8 +1,8 @@
-from collections.abc import Iterable, Iterator
+from collections.abc import Iterable, Sequence
 
 from .animator import Animator, Controller
 from .event import Header, to_json_lines, with_absolute_time
-from .repl import PyRepl
+from .repl import PyRepl, Repl
 
 
 def perform(
@@ -10,20 +10,40 @@ def perform(
     *,
     keypress_speed: float = 1.0,
     speed: float = 1.0,
-    width: int = 80,
-    height: int = 35,
+    width: int = 80,  # If non-positive, replaced by observed width of asciicast.
+    height: int = 35,  # If non-positive, replaced by observed height of asciicast.
     title: str = "Created by ascii-fx",
-) -> Iterator[str]:
+    AnimatorClass: type[Animator] = Animator,
+    ControllerClass: type[Controller] = Controller,
+    ReplClass: type[Repl] = PyRepl,
+) -> tuple[Sequence[str], int, int]:
+    """
+    Turn a script into an asciicast performance. This function feeds the given
+    script line by line into a new instance of the given REPL. It then animates
+    the resulting interactions with new instances of the given animator and
+    controller. Finally, it converts the resulting events into newline-delimited
+    JSON. This function returns the sequence of events as newline-delimited
+    JSON, the maximum number of columns taken up by the asciicast, and the
+    number of lines taken up by the asciicast.
+    """
     # Play back script in interpreter REPL.
-    repl = PyRepl()
+    repl = ReplClass()
     interactions = repl.simulate_all(lines)
 
     # Convert to events with timing information.
-    controller = Controller()
-    animator = Animator(controller, speed=speed, keypress_speed=keypress_speed)
+    controller = ControllerClass()
+    animator = AnimatorClass(controller, speed=speed, keypress_speed=keypress_speed)
     relative_events = animator.render_all(interactions)
+
+    # Fix width and height if necessary
+    if width <= 0:
+        width = animator.width + 1
+    if height <= 0:
+        height = animator.height + 1
 
     # Convert to newline-separated JSON.
     header = Header(width, height, title)
     absolute_events = with_absolute_time(relative_events)
-    return to_json_lines(header, absolute_events)
+    events = [*to_json_lines(header, absolute_events)]
+
+    return events, animator.width, animator.height
