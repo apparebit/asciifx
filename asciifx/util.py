65d277e2fea1681087ff66e205370051b430f5cf
Robert Grimm <apparebit@gmail.com>
2022-02-15 10:47:58 -0500

    Add method to strip ANSI escape codes

--- asciifx/util.py
+++ asciifx/util.py
@@ -1,6 +1,7 @@
 from collections import deque
 from collections.abc import Iterable, Iterator
-from typing import Any, Optional, TypeVar
+import re
+from typing import Any, TypeVar
 
 
 # Also see https://stackoverflow.com/questions/6822725/rolling-or-sliding-window-iterator#answer-6822761
@@ -17,3 +18,11 @@ def sliding_window(elements: Iterable[Any], size: int = 2) -> Iterator[tuple[Any
     for element in element_iter:
         append(element)
         yield tuple(window)
+
+
+# https://stackoverflow.com/questions/14693701/how-can-i-remove-the-ansi-escape-sequences-from-a-string-in-python
+ANSI_ESCAPE = re.compile(r'\x1b(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+
+
+def no_ansi_escape(text: str) -> str:
+    return ANSI_ESCAPE.sub("", text)
